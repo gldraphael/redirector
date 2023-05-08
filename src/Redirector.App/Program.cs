dfb7a173e34d0303f791b4fe68be7c8545cf34d5
Galdin Raphael <gldraphael@users.noreply.github.com>
2023-05-08 11:50:21 +0530

    Add support for reading from a config file

--- src/Redirector.App/Program.cs
+++ src/Redirector.App/Program.cs
@@ -1,6 +1,23 @@
 using Redirector.Api.Routes;
+using Redirector.App;
 
 var builder = WebApplication.CreateBuilder(args);
+var sources = builder.Configuration.GetSection("readRulesFrom").Get<RuleSources>();
+if(sources?.FileConfig is not null)
+{
+    var fileConfig = sources.FileConfig;
+    if(string.Equals(".json", Path.GetExtension(fileConfig), StringComparison.OrdinalIgnoreCase))
+    {
+        builder.Configuration.AddJsonFile(fileConfig);
+    }
+    else if (
+        string.Equals(".yaml", Path.GetExtension(fileConfig), StringComparison.OrdinalIgnoreCase)
+        || string.Equals(".yml", Path.GetExtension(fileConfig), StringComparison.OrdinalIgnoreCase))
+    {
+        builder.Configuration.AddYamlFile(fileConfig);
+    }
+}
+
 var app = builder.Build();
 app.MapRedirectRoutes();
 app.MapFallback(() => "Hello!");
