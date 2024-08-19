2d2109aeb6e2bb3fbedf780e67328d66bcf36a30
Galdin Raphael <gldraphael@users.noreply.github.com>
2024-08-19 12:17:50 +0530

    Make app configurable

--- app.go
+++ app.go
@@ -1,19 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-
-	var rules = GetRules("./rules.yaml")
+	
+	var rulesFile = getEnv("REDIRECTOR_RULES", "./rules.yaml");
+	var serverAddr = getEnv("SERVER_ADDRESS", "localhost:8080");
+	var rules = GetRules(rulesFile)
 
 	for _, rule := range rules {
 		http.HandleFunc(fmt.Sprintf("/%s", rule.Slug), func(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, rule.Target, http.StatusTemporaryRedirect)
-		})	
+		})
 	}
 
-    http.ListenAndServe("localhost:8000", nil)
+    http.ListenAndServe(serverAddr, nil)
+}
+
+func getEnv(key, fallback string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        value = fallback
+    }
+    return value
 }
