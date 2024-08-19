fc0e5e25e4cd43b0b37b4d05f936ccbeca5659d4
Galdin Raphael <gldraphael@users.noreply.github.com>
2024-08-19 13:04:43 +0530

    Add a working dockerfile (#12)

--- app.go
+++ app.go
@@ -8,16 +8,17 @@ import (
 
 func main() {
 	
-	var rulesFile = getEnv("REDIRECTOR_RULES", "./rules.yaml");
-	var serverAddr = getEnv("SERVER_ADDRESS", "localhost:8080");
-	var rules = GetRules(rulesFile)
+	var rulesFile  = getEnv("REDIRECTOR_RULES", 		 "./rules.yaml");
+	var serverAddr = getEnv("REDIRECTOR_SERVER_ADDRESS", "localhost:8080");
 
+	var rules = GetRules(rulesFile)
 	for _, rule := range rules {
 		http.HandleFunc(fmt.Sprintf("/%s", rule.Slug), func(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, rule.Target, http.StatusTemporaryRedirect)
 		})
 	}
 
+	println("redirector server listening at", serverAddr)
     http.ListenAndServe(serverAddr, nil)
 }
 
